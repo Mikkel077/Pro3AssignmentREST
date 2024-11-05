4cec08dde125580d71bc508bb483481aeb3275e4
mikkel077 <mikkelhansen077@gmail.com>
2024-11-05 14:08:20 +0100

    Initial commit

---
+++ src/main/java/com/pro3assignmentrest/Pro3AssignmentRestApplication.java
@@ -0,0 +1,13 @@
+package com.pro3assignmentrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro3AssignmentRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro3AssignmentRestApplication.class, args);
+	}
+
+}
